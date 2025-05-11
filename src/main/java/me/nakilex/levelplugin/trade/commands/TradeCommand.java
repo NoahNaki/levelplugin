bc759068d6f326456ad87002b066715956caf353
BuildTools <unconfigured@null.spigotmc.org>
2025-05-11 11:10:24 +0200

    trade distance

--- src/main/java/me/nakilex/levelplugin/trade/commands/TradeCommand.java
+++ src/main/java/me/nakilex/levelplugin/trade/commands/TradeCommand.java
@@ -1,137 +1,207 @@
 package me.nakilex.levelplugin.trade.commands;
 
-
 import me.nakilex.levelplugin.Main;
 import me.nakilex.levelplugin.utils.DealMaker;
 import me.nakilex.levelplugin.trade.utils.MessageStrings;
 import me.nakilex.levelplugin.trade.utils.Translations;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
-
 public class TradeCommand implements CommandExecutor {
+    private static final double MAX_DISTANCE = 10.0;
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
         MessageStrings messageStrings = Main.getPlugin().getMessageStrings();
 
-        if(sender instanceof Player) {
-            final String WRONG_USAGE = Main.PREFIX + messageStrings.getTranslation(Translations.WRONG_USAGE);
-            final String GITHUB_URL = "§6https://https://github.com/Robby3St/TradePlugin/§r";
-
+        if (sender instanceof Player) {
             Player p = (Player) sender;
-            if(Main.getPlugin().getConfigValues().USE_WITHOUT_PERMISSION || p.hasPermission("trade.trade")
+            final String WRONG_USAGE = Main.PREFIX + messageStrings.getTranslation(Translations.WRONG_USAGE);
+            final String GITHUB_URL = "§6https://github.com/Robby3St/TradePlugin/§r";
+
+            if (Main.getPlugin().getConfigValues().USE_WITHOUT_PERMISSION
+                || p.hasPermission("trade.trade")
                 || p.hasPermission("trade.*")) {
+
                 DealMaker dm = Main.getPlugin().getDealMaker();
-                if(args.length == 1) {
-                    if(args[0].equalsIgnoreCase("accept")) {
-                        dm.acceptTrade(p);
-                        return true;
 
+                if (args.length == 1) {
+                    String sub = args[0].toLowerCase();
 
-                    } else if(args[0].equalsIgnoreCase("cancel")) {
-                        dm.cancelOwnTrade(p);
-
-
-                    } else if(args[0].equalsIgnoreCase("deny")) {
-                        dm.denyTrade(p);
-
-
-                    } else if(Bukkit.getPlayer(args[0]) != null) {
-                        // Players sends trade request
-                        Player opposite = Bukkit.getPlayer(args[0]);
-                        boolean success = dm.makeTradeOffer(p, opposite);
-                        if(success)
-                            p.sendMessage(Main.PREFIX + String.format(messageStrings.getTranslation(Translations.TRADE_REQUEST_SENT), args[0]));
-
-
-                    } else if(args[0].equalsIgnoreCase("reload") && p.hasPermission("trade.reload")) {
-                        Main.getPlugin().reloadConfig();
-                        p.sendMessage(Main.PREFIX + messageStrings.getTranslation(Translations.RELOADED_CONFIG));
-
-
-                    } else if(args[0].equalsIgnoreCase("author")) {
-                        p.sendMessage(Main.PREFIX + messageStrings.getTranslation(Translations.AUTHOR_OF_PLUGIN_IS) + GITHUB_URL);
-
-
-                    } else if(args[0].equalsIgnoreCase("version") && (p.hasPermission("trade.version")
-                        || p.hasPermission("trade.*"))) {
-                        p.sendMessage(String.format(Main.PREFIX + messageStrings.getTranslation(Translations.PLUGIN_VERSION_IS),
-                            Main.getPlugin().getDescription().getVersion(), GITHUB_URL));
-
-
-                    } else if(args[0].equalsIgnoreCase("download")) {
-                        p.sendMessage(Main.PREFIX + messageStrings.getTranslation(Translations.DOWNLOAD_PLUGIN_HERE) + GITHUB_URL);
-
-
-                    } else if(args[0].equalsIgnoreCase("toggle")) {
-                        // Toggle use_without_permission_mode
-                        boolean useWithoutPermission = Main.getPlugin().getConfigValues().toggleUseWithoutPermission();
-                        p.sendMessage(String.format(Main.PREFIX +
-                            (useWithoutPermission ? messageStrings.getTranslation(
-                                Translations.YOU_ENABLED_USE_WITHOUT_PERMISSION) :
-                                messageStrings.getTranslation(
-                                    Translations.YOU_DISABLED_USE_WITHOUT_PERMISSION))));
-                    } else if(args[0].equalsIgnoreCase("block")) {
-                        // Block all trade requests
-                        if(!Main.getPlugin().getConfigValues().ALLOW_BLOCKING) p.sendMessage(Main.PREFIX + messageStrings.getTranslation(Translations.THIS_FEATURE_IS_NOT_ENABLED_IN_CONFIG));
-                        dm.blockAll(p);
-
-                    } else if(args[0].equalsIgnoreCase("unblock")) {
-                        // Block all trade requests
-                        dm.unblockAll(p);
-
-                    } else {
-                        p.sendMessage(String.format(messageStrings.getTranslation(
-                                Translations.COULD_NOT_FIND_PLAYER_WITH_THAT_NAME_PLEASE_USE_COMMAND),
-                            Main.PREFIX, args[0]));
-                    }
-                } else if(args.length == 2) {
-                    if(args[0].equalsIgnoreCase("accept")) {
-                        if(Bukkit.getPlayer(args[1]) != null) {
-                            dm.acceptTrade(p, Objects.requireNonNull(Bukkit.getPlayer(args[1])));
+                    switch (sub) {
+                        case "accept":
+                            dm.acceptTrade(p);
                             return true;
-                        } else {
-                            p.sendMessage(String.format(messageStrings.getTranslation(
-                                    Translations.COULD_NOT_FIND_PLAYER_WITH_THAT_NAME_PLEASE_USE_COMMAND),
-                                Main.PREFIX, args[1]));
-                        }
 
+                        case "cancel":
+                            dm.cancelOwnTrade(p);
+                            return true;
 
-                    } else if(args[0].equalsIgnoreCase("deny")) {
-                        if(Bukkit.getPlayer(args[1]) != null) {
-                            dm.denyTrade(p, Objects.requireNonNull(Bukkit.getPlayer(args[1])));
-                        } else {
-                            p.sendMessage(Main.PREFIX + messageStrings.getTranslation(Translations.COULD_NOT_FIND_PLAYER_WITH_THAT_NAME));
-                        }
-                    } else if(args[0].equalsIgnoreCase("block")) {
-                        // Block specific players
-                        if(!Main.getPlugin().getConfigValues().ALLOW_BLOCKING) p.sendMessage(Main.PREFIX + messageStrings.getTranslation(Translations.THIS_FEATURE_IS_NOT_ENABLED_IN_CONFIG));
-                        dm.addBlock(p, args[1].split(","));
+                        case "deny":
+                            dm.denyTrade(p);
+                            return true;
 
-                    } else if(args[0].equalsIgnoreCase("unblock")) {
-                        // Unblock specific players
-                        dm.addUnblock(p, args[1].split(","));
+                        case "reload":
+                            if (p.hasPermission("trade.reload")) {
+                                Main.getPlugin().reloadConfig();
+                                p.sendMessage(Main.PREFIX + messageStrings.getTranslation(Translations.RELOADED_CONFIG));
+                            } else {
+                                p.sendMessage(messageStrings.getTranslation(Translations.NO_PERMISSION));
+                            }
+                            return true;
+
+                        case "author":
+                            p.sendMessage(Main.PREFIX
+                                + messageStrings.getTranslation(Translations.AUTHOR_OF_PLUGIN_IS)
+                                + GITHUB_URL);
+                            return true;
+
+                        case "version":
+                            if (p.hasPermission("trade.version") || p.hasPermission("trade.*")) {
+                                p.sendMessage(String.format(
+                                    Main.PREFIX + messageStrings.getTranslation(Translations.PLUGIN_VERSION_IS),
+                                    Main.getPlugin().getDescription().getVersion(),
+                                    GITHUB_URL));
+                            } else {
+                                p.sendMessage(messageStrings.getTranslation(Translations.NO_PERMISSION));
+                            }
+                            return true;
+
+                        case "download":
+                            p.sendMessage(Main.PREFIX
+                                + messageStrings.getTranslation(Translations.DOWNLOAD_PLUGIN_HERE)
+                                + GITHUB_URL);
+                            return true;
+
+                        case "toggle":
+                            boolean useWithoutPermission = Main.getPlugin().getConfigValues().toggleUseWithoutPermission();
+                            p.sendMessage(Main.PREFIX + (useWithoutPermission
+                                ? messageStrings.getTranslation(Translations.YOU_ENABLED_USE_WITHOUT_PERMISSION)
+                                : messageStrings.getTranslation(Translations.YOU_DISABLED_USE_WITHOUT_PERMISSION)));
+                            return true;
+
+                        case "block":
+                            if (!Main.getPlugin().getConfigValues().ALLOW_BLOCKING) {
+                                p.sendMessage(Main.PREFIX + messageStrings.getTranslation(Translations.THIS_FEATURE_IS_NOT_ENABLED_IN_CONFIG));
+                            } else {
+                                dm.blockAll(p);
+                            }
+                            return true;
+
+                        case "unblock":
+                            dm.unblockAll(p);
+                            return true;
+
+                        default:
+                            // Assume player name
+                            Player target = Bukkit.getPlayer(args[0]);
+                            if (target != null) {
+                                if (isWithinDistance(p, target, MAX_DISTANCE)) {
+                                    boolean success = dm.makeTradeOffer(p, target);
+                                    if (success) {
+                                        p.sendMessage(Main.PREFIX + String.format(
+                                            messageStrings.getTranslation(Translations.TRADE_REQUEST_SENT),
+                                            target.getName()));
+                                    }
+                                } else {
+                                    p.sendMessage(Main.PREFIX
+                                        + "You must be within " + (int) MAX_DISTANCE + " blocks of that player to trade.");
+                                }
+                            } else {
+                                p.sendMessage(String.format(
+                                    messageStrings.getTranslation(
+                                        Translations.COULD_NOT_FIND_PLAYER_WITH_THAT_NAME_PLEASE_USE_COMMAND),
+                                    Main.PREFIX, args[0]));
+                            }
+                            return true;
                     }
+
+                } else if (args.length == 2) {
+                    String sub = args[0].toLowerCase();
+                    Player target = Bukkit.getPlayer(args[1]);
+
+                    switch (sub) {
+                        case "accept":
+                            if (target != null) {
+                                if (isWithinDistance(p, target, MAX_DISTANCE)) {
+                                    dm.acceptTrade(p, target);
+                                } else {
+                                    p.sendMessage(Main.PREFIX
+                                        + "You must be within " + (int) MAX_DISTANCE + " blocks of that player to trade.");
+                                }
+                            } else {
+                                p.sendMessage(String.format(
+                                    messageStrings.getTranslation(
+                                        Translations.COULD_NOT_FIND_PLAYER_WITH_THAT_NAME_PLEASE_USE_COMMAND),
+                                    Main.PREFIX, args[1]));
+                            }
+                            return true;
+
+                        case "deny":
+                            if (target != null) {
+                                dm.denyTrade(p, target);
+                            } else {
+                                p.sendMessage(Main.PREFIX
+                                    + messageStrings.getTranslation(Translations.COULD_NOT_FIND_PLAYER_WITH_THAT_NAME));
+                            }
+                            return true;
+
+                        case "block":
+                            if (!Main.getPlugin().getConfigValues().ALLOW_BLOCKING) {
+                                p.sendMessage(Main.PREFIX + messageStrings.getTranslation(Translations.THIS_FEATURE_IS_NOT_ENABLED_IN_CONFIG));
+                            } else if (target != null) {
+                                dm.addBlock(p, args[1].split(","));
+                            } else {
+                                p.sendMessage(Main.PREFIX
+                                    + messageStrings.getTranslation(Translations.COULD_NOT_FIND_PLAYER_WITH_THAT_NAME));
+                            }
+                            return true;
+
+                        case "unblock":
+                            if (target != null) {
+                                dm.addUnblock(p, args[1].split(","));
+                            } else {
+                                p.sendMessage(Main.PREFIX
+                                    + messageStrings.getTranslation(Translations.COULD_NOT_FIND_PLAYER_WITH_THAT_NAME));
+                            }
+                            return true;
+
+                        default:
+                            p.sendMessage(WRONG_USAGE);
+                            return true;
+                    }
+
                 } else {
                     p.sendMessage(WRONG_USAGE);
                 }
+
             } else {
                 p.sendMessage(messageStrings.getTranslation(Translations.NO_PERMISSION));
             }
+
         } else {
-            if(args[0].equalsIgnoreCase("reload")) {
+            // Console or non-player sender
+            if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
                 Main.getPlugin().reloadConfig();
                 Main.getPlugin().reloadConfigValues();
                 Main.getPlugin().getMessageStrings().reloadConfig();
                 sender.sendMessage(messageStrings.getTranslation(Translations.RELOADED_CONFIG));
-            } else
+            } else {
                 sender.sendMessage(messageStrings.getTranslation(Translations.MUST_BE_A_PLAYER));
+            }
         }
         return true;
+    }
+
+    private boolean isWithinDistance(Player a, Player b, double maxDistance) {
+        if (!a.getWorld().equals(b.getWorld())) return false;
+        Location locA = a.getLocation();
+        Location locB = b.getLocation();
+        return locA.distance(locB) <= maxDistance;
     }
 }
