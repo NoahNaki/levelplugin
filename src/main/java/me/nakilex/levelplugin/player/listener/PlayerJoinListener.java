ffc077927a6309ac0a0cb9d7ba0a6ecd03897caf
BuildTools <unconfigured@null.spigotmc.org>
2025-05-17 10:34:06 +0200

    gm adventure on join

--- src/main/java/me/nakilex/levelplugin/player/listener/PlayerJoinListener.java
+++ src/main/java/me/nakilex/levelplugin/player/listener/PlayerJoinListener.java
@@ -23,15 +23,9 @@ public class PlayerJoinListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         final Player player = event.getPlayer();
 
-        // Initial debug log
-        Main.getInstance().getLogger().info("[PlayerJoinListener] onPlayerJoin fired for player: " + player.getName());
-        player.sendMessage("[Debug] onPlayerJoin called");
-
         // Delay setting the gamemode to ensure no other plugin overrides it
         Bukkit.getScheduler().runTaskLater(Main.getInstance(), () -> {
             player.setGameMode(GameMode.ADVENTURE);
-            Main.getInstance().getLogger().info("[PlayerJoinListener] Delayed Set game mode to: " + player.getGameMode());
-            player.sendMessage("[Debug] Delayed GameMode is now: " + player.getGameMode());
 
             // Recalculate stats to apply the proper health scaling
             StatsManager.getInstance().recalcDerivedStats(player);
@@ -40,8 +34,6 @@ public class PlayerJoinListener implements Listener {
             // Apply health scaling and update scoreboard
             player.setHealthScaled(true);
             player.setHealthScale(20.0);
-            Main.getInstance().getLogger().info("[PlayerJoinListener] Health scaled and set to 20.0 for player: " + player.getName());
-            player.sendMessage("[Debug] Health scaled: " + player.getHealthScale());
         }, 2L); // delay of 2 ticks (~0.1 seconds)
     }
 }
