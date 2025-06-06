0f011ee2a8b77ca5c0789cb90682bf822e013f51
NoahNaki <74353773+NoahNaki@users.noreply.github.com>
2025-06-06 18:56:37 +0200

    Ignore stamina when in creative mode

--- src/main/java/me/nakilex/levelplugin/player/attributes/managers/StaminaTask.java
+++ src/main/java/me/nakilex/levelplugin/player/attributes/managers/StaminaTask.java
@@ -1,6 +1,7 @@
 package me.nakilex.levelplugin.player.attributes.managers;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -17,6 +18,15 @@ public class StaminaTask extends BukkitRunnable {
             StaminaManager mgr = StaminaManager.getInstance();
             double max = mgr.getMaxStamina(player);
             double current = mgr.getStamina(player);
+
+            if (player.getGameMode() == GameMode.CREATIVE) {
+                mgr.setStamina(player, max);
+                if (player.getFoodLevel() != 20) {
+                    player.setFoodLevel(20);
+                }
+                player.setSaturation(0f);
+                continue;
+            }
 
             if (player.isSprinting()) {
                 current -= DRAIN_RATE;
