3c512f575b384feecfe90db86aed077acf895724
NoahNaki <74353773+NoahNaki@users.noreply.github.com>
2025-06-11 23:38:40 +0200

    Tweak shockwave block height and add sweep cooldown

--- src/main/java/me/nakilex/levelplugin/spells/effect/warrior/ShockwaveEffect.java
+++ src/main/java/me/nakilex/levelplugin/spells/effect/warrior/ShockwaveEffect.java
@@ -71,7 +71,8 @@ public class ShockwaveEffect implements SpellEffect {
                             Location bLoc = ground.getLocation().add(0.5, 1.0, 0.5);
                             FallingBlock fb = loc.getWorld().spawnFallingBlock(bLoc, ground.getBlockData());
                             fb.setDropItem(false);
-                            fb.setVelocity(new Vector(Math.cos(rad) * 0.2, 0.4, Math.sin(rad) * 0.2));
+                            // Slightly lower upward velocity so blocks don't fly as high
+                            fb.setVelocity(new Vector(Math.cos(rad) * 0.2, 0.25, Math.sin(rad) * 0.2));
                             fb.setMetadata("Shockwave", new FixedMetadataValue(plugin, true));
                             Bukkit.getScheduler().runTaskLater(plugin, fb::remove, 40L);
                         }
