09f160b5f91b4a0e5cc5be2b75bac8e65fd32f76
NoahNaki <74353773+NoahNaki@users.noreply.github.com>
2025-06-16 15:05:51 +0200

    Add debug log for ArrowShowerEffect

--- src/main/java/me/nakilex/levelplugin/spells/effect/archer/ArrowShowerEffect.java
+++ src/main/java/me/nakilex/levelplugin/spells/effect/archer/ArrowShowerEffect.java
@@ -5,6 +5,7 @@ import me.nakilex.levelplugin.epicspells.SpellRunner;
 import me.nakilex.levelplugin.epicspells.spells.ArrowStorm;
 import me.nakilex.levelplugin.spells.context.SpellCastContext;
 import me.nakilex.levelplugin.spells.effect.SpellEffect;
+import me.nakilex.levelplugin.Main;
 import org.bukkit.entity.Player;
 
 /**
@@ -15,6 +16,7 @@ public class ArrowShowerEffect implements SpellEffect {
     @Override
     public void apply(SpellCastContext ctx) {
         Player player = ctx.getPlayer();
+        Main.getInstance().getLogger().info("[DBG] ArrowShowerEffect triggered for " + player.getName());
         ArrowStorm spell = new ArrowStorm();
         spell.init(new SpellManager(), player, 0, 0, "Arrow Storm");
         spell.setAlive(true);
