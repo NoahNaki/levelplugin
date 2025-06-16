69cd36c996e7ef3f5399b05ae25732862e5e07ca
NoahNaki <74353773+NoahNaki@users.noreply.github.com>
2025-06-16 14:56:02 +0200

    Add Arrow Shower effect and rune

---
+++ src/main/java/me/nakilex/levelplugin/spells/effect/archer/ArrowShowerEffect.java
@@ -0,0 +1,23 @@
+package me.nakilex.levelplugin.spells.effect.archer;
+
+import me.nakilex.levelplugin.epicspells.SpellManager;
+import me.nakilex.levelplugin.epicspells.SpellRunner;
+import me.nakilex.levelplugin.epicspells.spells.ArrowStorm;
+import me.nakilex.levelplugin.spells.context.SpellCastContext;
+import me.nakilex.levelplugin.spells.effect.SpellEffect;
+import org.bukkit.entity.Player;
+
+/**
+ * Wraps the legacy ArrowStorm spell from the epicspells package so it can
+ * be triggered through the normal spell system (via a rune transform).
+ */
+public class ArrowShowerEffect implements SpellEffect {
+    @Override
+    public void apply(SpellCastContext ctx) {
+        Player player = ctx.getPlayer();
+        ArrowStorm spell = new ArrowStorm();
+        spell.init(new SpellManager(), player, 0, 0, "Arrow Storm");
+        spell.setAlive(true);
+        new SpellRunner(spell).start();
+    }
+}
