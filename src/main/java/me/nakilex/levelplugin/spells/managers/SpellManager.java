c12c17834762b9a77dbdeb762b3b4772c8aaf340
NoahNaki <74353773+NoahNaki@users.noreply.github.com>
2025-06-25 18:02:03 +0200

    Ensure effects registered before loading spells

--- src/main/java/me/nakilex/levelplugin/spells/managers/SpellManager.java
+++ src/main/java/me/nakilex/levelplugin/spells/managers/SpellManager.java
@@ -36,6 +36,8 @@ public class SpellManager {
         instance = this;
         this.plugin = plugin;
         this.effectRegistry = EffectRegistry.getInstance();
+        // Ensure effects are registered before any spells try to use them
+        EffectRegistry.registerAll();
         loadSpells();
     }
 
