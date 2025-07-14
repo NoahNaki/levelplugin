4fea67fee015ba046a3c8eb1eeed23193ec5d196
NoahNaki <74353773+NoahNaki@users.noreply.github.com>
2025-07-14 22:44:23 +0200

    Use ChunkLoadEvent for environment reload

--- src/main/java/me/nakilex/levelplugin/environment/listeners/EnvironmentChunkListener.java
+++ src/main/java/me/nakilex/levelplugin/environment/listeners/EnvironmentChunkListener.java
@@ -1,9 +1,9 @@
 package me.nakilex.levelplugin.environment.listeners;
 
 import me.nakilex.levelplugin.environment.EnvironmentManager;
-import io.papermc.paper.event.player.PlayerChunkLoadEvent;
 import org.bukkit.Chunk;
 import org.bukkit.entity.Player;
+import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -16,21 +16,20 @@ public class EnvironmentChunkListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onChunkLoad(PlayerChunkLoadEvent event) {
-        Player player = event.getPlayer();
+    public void onChunkLoad(ChunkLoadEvent event) {
         Chunk chunk = event.getChunk();
-        int cx = chunk.getX();
-        int cz = chunk.getZ();
 
-        // Re-send fake blocks for this chunk to the player now that it is
-        // actually sent to them
-        environmentManager.handleChunkLoad(player, chunk);
+        for (Player player : chunk.getWorld().getPlayers()) {
+            int cx = chunk.getX();
+            int cz = chunk.getZ();
 
-        // If this chunk contains the player's settlement origin, respawn all
-        // structures instantly so they appear correctly
-        var origin = environmentManager.getOrigin(player.getUniqueId());
-        if (origin != null && origin.getChunk().getX() == cx && origin.getChunk().getZ() == cz) {
-            environmentManager.initializePlayer(player);
+            // resend fake blocks contained in this chunk
+            environmentManager.handleChunkLoad(player, chunk);
+
+            var origin = environmentManager.getOrigin(player.getUniqueId());
+            if (origin != null && origin.getChunk().getX() == cx && origin.getChunk().getZ() == cz) {
+                environmentManager.initializePlayer(player);
+            }
         }
     }
 }
