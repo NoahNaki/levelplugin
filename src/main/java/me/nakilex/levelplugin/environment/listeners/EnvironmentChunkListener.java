4ff255cce0a9620f829cfc3c0803e0c64e3da3d9
NoahNaki <74353773+NoahNaki@users.noreply.github.com>
2025-07-14 21:50:46 +0200

    Handle chunk loads without Paper event

--- src/main/java/me/nakilex/levelplugin/environment/listeners/EnvironmentChunkListener.java
+++ src/main/java/me/nakilex/levelplugin/environment/listeners/EnvironmentChunkListener.java
@@ -1,10 +1,12 @@
 package me.nakilex.levelplugin.environment.listeners;
 
 import me.nakilex.levelplugin.environment.EnvironmentManager;
-import io.papermc.paper.event.player.PlayerChunkLoadEvent;
+import org.bukkit.Chunk;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.world.ChunkLoadEvent;
 
 public class EnvironmentChunkListener implements Listener {
     private final EnvironmentManager environmentManager;
@@ -14,7 +16,17 @@ public class EnvironmentChunkListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onChunkLoad(PlayerChunkLoadEvent event) {
-        environmentManager.handleChunkLoad(event.getPlayer());
+    public void onChunkLoad(ChunkLoadEvent event) {
+        Chunk chunk = event.getChunk();
+        int cx = chunk.getX();
+        int cz = chunk.getZ();
+        int view = chunk.getWorld().getViewDistance();
+        for (Player player : chunk.getWorld().getPlayers()) {
+            int pcx = player.getLocation().getChunk().getX();
+            int pcz = player.getLocation().getChunk().getZ();
+            if (Math.abs(pcx - cx) <= view && Math.abs(pcz - cz) <= view) {
+                environmentManager.handleChunkLoad(player);
+            }
+        }
     }
 }
