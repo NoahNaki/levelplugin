fa6c4b4d585314c9daf7f9e3d63f40574650b3df
NoahNaki <74353773+NoahNaki@users.noreply.github.com>
2025-07-19 22:51:58 +0200

    Add debug logging for building hologram clicks

--- src/main/java/me/nakilex/levelplugin/environment/listeners/BuildingHologramListener.java
+++ src/main/java/me/nakilex/levelplugin/environment/listeners/BuildingHologramListener.java
@@ -1,5 +1,6 @@
 package me.nakilex.levelplugin.environment.listeners;
 
+import me.nakilex.levelplugin.Main;
 import me.nakilex.levelplugin.environment.BuildingUpgradeGUI;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.TextDisplay;
@@ -18,15 +19,20 @@ public class BuildingHologramListener implements Listener {
     @EventHandler(priority = org.bukkit.event.EventPriority.HIGHEST, ignoreCancelled = true)
     public void onInteract(PlayerInteractEntityEvent event) {
         var entity = event.getRightClicked();
+        Main.getPlugin().getLogger().info("[DBG] BuildingHologramListener triggered by "
+                + event.getPlayer().getName() + " on " + entity.getType());
         if (entity instanceof ArmorStand || entity instanceof TextDisplay) {
             for (String tag : entity.getScoreboardTags()) {
+                Main.getPlugin().getLogger().info("[DBG] checking tag " + tag);
                 if (tag.startsWith("building_hologram:")) {
                     String building = tag.substring("building_hologram:".length());
+                    Main.getPlugin().getLogger().info("[DBG] opening building GUI for " + building);
                     event.setCancelled(true);
                     gui.open(event.getPlayer(), building);
                     return;
                 }
             }
         }
+        Main.getPlugin().getLogger().info("[DBG] entity was not a building hologram");
     }
 }
