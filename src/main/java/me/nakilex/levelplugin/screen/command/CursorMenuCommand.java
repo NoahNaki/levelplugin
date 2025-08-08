e919b3fb0c6113a9eb659b455cb4e9cd8d19099c
NoahNaki <74353773+NoahNaki@users.noreply.github.com>
2025-08-08 10:14:00 +0200

    Improve cursor menu command feedback

--- src/main/java/me/nakilex/levelplugin/screen/command/CursorMenuCommand.java
+++ src/main/java/me/nakilex/levelplugin/screen/command/CursorMenuCommand.java
@@ -1,6 +1,8 @@
 package me.nakilex.levelplugin.screen.command;
 
 import me.nakilex.levelplugin.screen.CursorMenuSystem;
+import me.nakilex.levelplugin.screen.menu.Section;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -24,17 +26,45 @@ public class CursorMenuCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (!(sender instanceof Player player)) return true;
-        if (args.length == 0) return false;
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(ChatColor.RED + "Only players may use this command.");
+            return true;
+        }
+
+        if (args.length == 0) {
+            sender.sendMessage(ChatColor.YELLOW + "Usage: /cursormenu <run|stop>");
+            return true;
+        }
+
         switch (args[0].toLowerCase()) {
             case "run" -> {
-                if (args.length > 1) {
-                    system.setupCursor(player, args[1]);
+                if (args.length < 2) {
+                    sender.sendMessage(ChatColor.YELLOW + "Usage: /cursormenu run <section>");
+                    return true;
                 }
+
+                Section section = system.getSectionManager().get(args[1]);
+                if (section == null) {
+                    sender.sendMessage(ChatColor.RED + "Unknown section: " + args[1]);
+                    return true;
+                }
+                if (section.getPermission() != null && !section.getPermission().isEmpty() && !player.hasPermission(section.getPermission())) {
+                    sender.sendMessage(ChatColor.RED + "You do not have permission for this menu.");
+                    return true;
+                }
+
+                system.setupCursor(player, section.getKey());
+                sender.sendMessage(ChatColor.GREEN + "Opened cursor menu: " + section.getKey());
             }
-            case "stop" -> system.stopCursor(player);
-            default -> {
+            case "stop" -> {
+                if (!system.isInMenu(player)) {
+                    sender.sendMessage(ChatColor.RED + "You are not using a cursor menu.");
+                    return true;
+                }
+                system.stopCursor(player);
+                sender.sendMessage(ChatColor.YELLOW + "Closed cursor menu.");
             }
+            default -> sender.sendMessage(ChatColor.YELLOW + "Usage: /cursormenu <run|stop>");
         }
         return true;
     }
