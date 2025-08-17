70fa205dc0897ec8a96d3a98fc220cda4478e788
BuildTools <unconfigured@null.spigotmc.org>
2025-08-18 00:11:12 +0200

    meme

--- src/main/java/me/nakilex/levelplugin/player/attributes/managers/StatsManager.java
+++ src/main/java/me/nakilex/levelplugin/player/attributes/managers/StatsManager.java
@@ -219,7 +219,7 @@ public class StatsManager {
         player.setWalkSpeed(newWalkSpeed);
 
         ps.attackSpeed = 0.5 * (1.0 + 0.01 * (ps.baseTechnique + ps.bonusTechnique));
-        AttributeInstance atkAttr = player.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
+        AttributeInstance atkAttr = player.getAttribute(Attribute.ATTACK_SPEED);
         if (atkAttr != null) atkAttr.setBaseValue(ps.attackSpeed * 8.0);
     }
 
