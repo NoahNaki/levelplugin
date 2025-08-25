024709036af714c2a41128213422258baf9c107d
NoahNaki <74353773+NoahNaki@users.noreply.github.com>
2025-08-25 16:48:49 +0200

    Add duel request command

--- src/main/java/me/nakilex/levelplugin/duels/commands/DuelCommand.java
+++ src/main/java/me/nakilex/levelplugin/duels/commands/DuelCommand.java
@@ -1,15 +1,24 @@
 package me.nakilex.levelplugin.duels.commands;
 
+import me.nakilex.levelplugin.duels.listeners.DuelListener;
 import me.nakilex.levelplugin.duels.managers.DuelManager;
+import me.nakilex.levelplugin.duels.managers.DuelRequest;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import me.nakilex.levelplugin.utils.CommandUtil;
 
 import static me.nakilex.levelplugin.utils.ChatMessageUtil.MessageType;
 import static me.nakilex.levelplugin.utils.ChatMessageUtil.send;
 
-public class DuelCommand implements CommandExecutor {
+public class DuelCommand implements TabExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -20,27 +29,76 @@ public class DuelCommand implements CommandExecutor {
         Player player = (Player) sender;
 
         if (args.length == 0) {
-            send(player, MessageType.INFO, "Usage: /duel <accept|decline>");
+            send(player, MessageType.INFO, "Usage: /duel <player|accept|decline>");
             return true;
         }
 
-        if (args[0].equalsIgnoreCase("accept")) {
-            boolean accepted = DuelManager.getInstance().acceptRequest(player);
+        String sub = args[0];
+        DuelManager manager = DuelManager.getInstance();
+
+        if (sub.equalsIgnoreCase("accept")) {
+            boolean accepted = manager.acceptRequest(player);
             if (accepted) {
                 send(player, MessageType.SUCCESS, "You have accepted the duel request!");
             } else {
                 send(player, MessageType.ERROR, "You have no valid duel request to accept!");
             }
-        } else if (args[0].equalsIgnoreCase("decline")) {
-            boolean declined = DuelManager.getInstance().declineRequest(player);
+            return true;
+        }
+
+        if (sub.equalsIgnoreCase("decline")) {
+            boolean declined = manager.declineRequest(player);
             if (declined) {
                 send(player, MessageType.ERROR, "You have declined the duel request!");
             } else {
                 send(player, MessageType.ERROR, "You have no valid duel request to decline!");
             }
-        } else {
-            send(player, MessageType.INFO, "Usage: /duel <accept|decline>");
+            return true;
         }
+
+        Player target = Bukkit.getPlayerExact(sub);
+        if (target == null) {
+            send(player, MessageType.ERROR, "Player not found.");
+            return true;
+        }
+        if (target.equals(player)) {
+            send(player, MessageType.ERROR, "You cannot duel yourself.");
+            return true;
+        }
+
+        if (manager.areInAnyDuel(player) || manager.areInAnyDuel(target)) {
+            send(player, MessageType.ERROR, "Either you or the target is already in a duel.");
+            return true;
+        }
+
+        DuelRequest incoming = manager.getRequest(player.getUniqueId());
+        if (incoming != null && incoming.getRequester().equals(target.getUniqueId())) {
+            manager.acceptRequest(player);
+            send(player, MessageType.SUCCESS, "You have accepted the duel request from " + target.getName() + "!");
+            send(target, MessageType.SUCCESS, player.getName() + " has accepted your duel request!");
+            return true;
+        }
+
+        if (manager.getRequest(target.getUniqueId()) != null) {
+            send(player, MessageType.ERROR, "That player already has a pending duel request.");
+            return true;
+        }
+
+        manager.createRequest(player, target);
+        send(player, MessageType.SUCCESS, "Duel request sent to " + target.getName() + "!");
+        new DuelListener().sendDuelRequestMessage(target, player.getName());
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 1) {
+            List<String> suggestions = new ArrayList<>(CommandUtil.onlinePlayerNames(args[0]));
+            String lower = args[0].toLowerCase();
+            if ("accept".startsWith(lower)) suggestions.add("accept");
+            if ("decline".startsWith(lower)) suggestions.add("decline");
+            return suggestions;
+        }
+        return Collections.emptyList();
     }
 }
