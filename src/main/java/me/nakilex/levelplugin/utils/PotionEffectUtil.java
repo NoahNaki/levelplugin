e94efc40060e9dd73e3b6b48f2c2a3bf3a2fcdd4
Naki <74353773+NoahNaki@users.noreply.github.com>
2025-12-30 21:46:25 +0100

    Control swing animation with hidden effects

--- src/main/java/me/nakilex/levelplugin/utils/PotionEffectUtil.java
+++ src/main/java/me/nakilex/levelplugin/utils/PotionEffectUtil.java
@@ -2,6 +2,7 @@ package me.nakilex.levelplugin.utils;
 
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 /** Utility methods for handling potion effects. */
 public final class PotionEffectUtil {
@@ -12,5 +13,19 @@ public final class PotionEffectUtil {
         for (PotionEffect effect : entity.getActivePotionEffects()) {
             entity.removePotionEffect(effect.getType());
         }
+    }
+
+    /** Apply a hidden potion effect, replacing any existing one of the same type. */
+    public static void applyHiddenEffect(LivingEntity entity, PotionEffectType type, int durationTicks, int amplifier) {
+        if (durationTicks <= 0) {
+            return;
+        }
+        PotionEffect effect = new PotionEffect(type, durationTicks, amplifier, false, false, false);
+        entity.addPotionEffect(effect, true);
+    }
+
+    /** Remove a specific potion effect if present. */
+    public static void removeEffect(LivingEntity entity, PotionEffectType type) {
+        entity.removePotionEffect(type);
     }
 }
