0101a85b9d51f4709a17c665a15f7e44daea784d
Naki <74353773+NoahNaki@users.noreply.github.com>
2026-01-18 20:30:35 +0100

    Initialize NPC registry before server selection

--- src/main/java/me/nakilex/levelplugin/core/PluginBootstrap.java
+++ src/main/java/me/nakilex/levelplugin/core/PluginBootstrap.java
@@ -74,6 +74,8 @@ import me.nakilex.levelplugin.utils.registeries.ListenerRegistry;
 import me.nakilex.levelplugin.utils.registeries.TaskRegistry;
 import me.nakilex.levelplugin.fasttravel.FastTravelManager;
 import me.nakilex.levelplugin.fasttravel.gui.FastTravelGUI;
+import me.nakilex.levelplugin.npc.system.NpcApi;
+import me.nakilex.levelplugin.npc.system.NpcRegistry;
 import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.npc.NPC;
 import org.bukkit.Bukkit;
@@ -295,6 +297,7 @@ public class PluginBootstrap {
                 ? customConfig.getString("server.hub-world", "hub")
                 : "hub";
         worldManager.ensureWorldsLoaded("flatland", "redrocks", hubWorld);
+        NpcApi.initialize(new NpcRegistry(plugin));
         serverSelectionManager = new me.nakilex.levelplugin.server.ServerSelectionManager(plugin);
 
         itemManager = new ItemManager(plugin);
