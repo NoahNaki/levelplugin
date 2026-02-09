cadfce84a703998366e261e3a04490642c9eb63e
Naki <74353773+NoahNaki@users.noreply.github.com>
2026-02-09 14:55:03 +0100

    Add pet gacha pulls and inventory settings

--- src/main/java/me/nakilex/levelplugin/core/PluginBootstrap.java
+++ src/main/java/me/nakilex/levelplugin/core/PluginBootstrap.java
@@ -599,6 +599,7 @@ public class PluginBootstrap {
             serverSelectionManager,
             petManager,
             petGUI,
+            petSettingsGUI,
             customMobManager,
             arcSlashDebugManager,
             arcSlashDebugGUI
