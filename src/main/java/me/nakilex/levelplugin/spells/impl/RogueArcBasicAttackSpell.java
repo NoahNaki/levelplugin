77ace742fbc6c7ea761d97565cd0690fc0a2918b
Naki <74353773+NoahNaki@users.noreply.github.com>
2026-03-22 23:15:43 +0100

    Add direct lock-on damage fallback for rogue basic slash

--- src/main/java/me/nakilex/levelplugin/spells/impl/RogueArcBasicAttackSpell.java
+++ src/main/java/me/nakilex/levelplugin/spells/impl/RogueArcBasicAttackSpell.java
@@ -2,8 +2,11 @@ package me.nakilex.levelplugin.spells.impl;
 
 import me.nakilex.levelplugin.spells.ArcSlashCombatUtil;
 import me.nakilex.levelplugin.spells.SpellContext;
+import me.nakilex.levelplugin.spells.SpellEffectUtil;
 import me.nakilex.levelplugin.spells.SpellHandler;
+import me.nakilex.levelplugin.spells.SpellTargetingUtil;
 import org.bukkit.Sound;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
@@ -26,6 +29,14 @@ public class RogueArcBasicAttackSpell implements SpellHandler {
         lastCastAt.put(caster.getUniqueId(), now);
 
         ArcSlashCombatUtil.strikeForward(caster, 1.2, 1.0, 3.4, 1.55);
+        LivingEntity lockedTarget = SpellTargetingUtil.resolveTargetLivingEntity(caster, 4.5, 1.1,
+                living -> !living.equals(caster));
+        if (lockedTarget != null) {
+            SpellEffectUtil.applyDirectSpellDamage(context.plugin(), caster, lockedTarget, 3.4, true);
+        } else {
+            SpellEffectUtil.applyAreaDamage(caster, caster.getLocation().clone().add(caster.getLocation().getDirection().setY(0.0).normalize().multiply(1.8)),
+                    2.0, 3.0);
+        }
         caster.getWorld().playSound(caster.getLocation(), Sound.ENTITY_PLAYER_ATTACK_SWEEP, 0.65f, 1.35f);
     }
 }
